a1ba6d8f7072c58cb5ed340536b8b912b514a986
Syifatf <syifatf@gmail.com>
2020-11-21 08:09:13 +0700

    tutorial belajar

--- src/script/component/app-bar.js
+++ src/script/component/app-bar.js
@@ -1,11 +1,9 @@
-//buat class yang mewarisi sifat HTMLELement
-class AppBar extends HTMLElement {
-  constructor() {
-    super();
-    this.shadowDOM = this.attachShadow({ mode: "open" });
-  }
-  //akan terpanggil ketika element
-  //diterapkan pada DOM
+class Header extends HTMLElement {
+  // constructor() {
+  //   super();
+  //   this.shadowDOM = this.attachShadow({ mode: "open" });
+  // }
+  //akan terpanggil ketika element diterapkan pada DOM
   connectedCallback() {
     //jika ingin element diterapkan langsung
     this.render();
@@ -15,28 +13,37 @@ class AppBar extends HTMLElement {
     //berfungsi untuk menampilkan element yg dibutuhkan
     //melalui property
     //lihat this.innerHTML
-    this.shadowDOM.innerHTML = `
-    <style>
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
-    :host {
-      display: block;
-      width: 100%;
-      background-color: cornflowerblue;
-      color: white;
-      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    }
-    h2 {
-      padding: 16px;
-    }
-    </style>
-    <h2>Club Finder</h2>`;
+    this.innerHTML = `
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><img src="assets/image/meals.png" width="30" height="30" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+             
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+          <a class="nav-link" href="#carouselExampleControls">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active   ">
+          <a class="nav-link" href="#data">Meal<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active   ">
+          <a class="nav-link" href="#data2">Drink<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active   ">
+          <a class="nav-link" href="../src/menu2.html">MealDB<span class="sr-only">(current)</span></a>
+        </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="search" aria-label="search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">search</button>
+          </form>
+        </div>
+      </nav>`;
   }
 }
 
 //definisikan custom element agar bisa
 //digunakan pada DOM
-customElements.define("app-bar", AppBar);
+customElements.define("nav-header", Header);
